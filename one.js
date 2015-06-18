65c0cdc092f37e17f8d92f14575640f817d7fcbc
maksimgm <maksimmamrikov@gmail.com>
2015-06-18 11:33:18 -0700

    add function declaration for box.

--- one.js
+++ one.js
@@ -1,11 +1,27 @@
-<script>
+//changing DOMs
 
-document.getElementById("box").onclick = changeBackground;
 
-function changeBackground() {
-  document.head.style.background = "red";
+//document.getElementById("box").onclick("box").style.backgroundColor = red;
+// var box = document.getElementById("box");
+// box.onclick=function(){
+//   console.log('yay, it worked!');
+// };
+
+//function myFirstFunction( age, hobby ) {
+//console.log('when I reach ' + age + ' I will be good at ' + hobby);
+//}
+
+//myFirstFunction( 100, 'cooking');
+box.onclick = myFunction;
+
+function myFunction(){
+  document.getElementById("box") .style.background = "red";
 }
 
 
 
-</script>
+
+// function declarations
+// calling the fucnction
+// working with JS data types
+// onclick events
