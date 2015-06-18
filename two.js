b91a1dcc71b34af530943ab415ef779dfaee38d8
maksimgm <maksimmamrikov@gmail.com>
2015-06-18 13:44:33 -0700

    add html and js doc

---
+++ two.js
@@ -0,0 +1,33 @@
+
+//document.getElementById("box").onclick("box").style.backgroundColor = red;
+// var box = document.getElementById("box");
+// box.onclick=function(){
+//   console.log('yay, it worked!');
+// };
+
+//function myFirstFunction( age, hobby ) {
+//console.log('when I reach ' + age + ' I will be good at ' + hobby);
+//}
+
+//myFirstFunction( 100, 'cooking');
+//box.onclick = myFunction;
+
+//function myFunction(){
+//  document.getElementById("box") .style.background = "red";
+//}
+
+//myFunction;
+
+
+text.onclick = myFunction;
+
+function myFunction() {
+  //document.getElementById("text");
+  document.write("<p>Luke I am your father</p>");
+}
+
+
+
+
+
+myFunction;
