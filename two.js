104266304c13ccffaa8d81ef037c7cc26d3899a4
maksimgm <maksimmamrikov@gmail.com>
2015-06-19 11:24:00 -0700

    add js script

--- two.js
+++ two.js
@@ -1,50 +1,7 @@
+var pox = document.getElementById("pox");
 
-//document.getElementById("box").onclick("box").style.backgroundColor = red;
-// var box = document.getElementById("box");
-// box.onclick=function(){
-//   console.log('yay, it worked!');
-// };
+pox.onclick = function myFunction() {
+  document.getElementById("juice");
+  juice.innerHTML = ("This is the internet talking to you. ")
+};
 
-//function myFirstFunction( age, hobby ) {
-//console.log('when I reach ' + age + ' I will be good at ' + hobby);
-//}
-
-//myFirstFunction( 100, 'cooking');
-//box.onclick = myFunction;
-
-//function myFunction(){
-//  document.getElementById("box") .style.background = "red";
-//}
-
-//myFunction;
-
-//text.onclick = myFunction;
-
-//function () {
-  //document.getElementById("text") .style.display = "Hello World";
-  
-//}
-
-//myFunction;
-
-
-/*if (document.onclick) {
-  getElementById("text").write = "yellow";
-} else{
-  getElementById("text").write = "black";
-}*/
-
-/*function add (text){
-  var myVar = document.getElementById("juice");
-  myVar.value = "hello world";
-}
-
-add;*/
-
-juice.onclick = myFirstFunction;
-
-function myFirstFunction(text){
-  document.getElementById("juice").style.display = "Hello World";
-}
-
-myFirstFunction;
