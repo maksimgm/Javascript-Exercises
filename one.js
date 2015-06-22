44a85042928edc17a003cb0307ef107fa32313d5
maksimgm <maksimmamrikov@gmail.com>
2015-06-21 18:47:19 -0700

    clean up dead code. Fix indentation.

--- one.js
+++ one.js
@@ -1,34 +1,6 @@
-//changing DOMs
-
-
-//document.getElementById("box").onclick("box").style.backgroundColor = red;
-// var box = document.getElementById("box");
-// box.onclick=function(){
-//   console.log('yay, it worked!');
-// };
-
-//function myFirstFunction( age, hobby ) {
-//console.log('when I reach ' + age + ' I will be good at ' + hobby);
-//}
-
-//myFirstFunction( 100, 'cooking');
- // box.onclick = myFunction;
-
- // function myFunction(){
- //   document.getElementById("box").style.background = "red";
- // }
-
- // myFunction();
 
 var box = document.getElementById("box");
+
 box.onclick = function(){
- box.style.background = 'green';
+  box.style.background = 'green';
 };
-
-
-
-
-// function declarations
-// calling the fucnction
-// working with JS data types
-// onclick events
