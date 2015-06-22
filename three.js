52f47cf54c389420d68ad124c282acb628599670
maksimgm <maksimmamrikov@gmail.com>
2015-06-21 19:03:18 -0700

    remove newDiv. onclick adds h3 into h1. gitignore file added.

--- three.js
+++ three.js
@@ -3,11 +3,6 @@ var wrapper = document.getElementById("wrapper");
 wrapper.onclick = function MyFunc () {
   document.getElementById("candy");
   candy.innerHTML = "<h3> Because candy tastes amazing! </h3>";
-  var newDiv = document.createElement('div');
-  document.body.appendChild(newDiv);
-  newDiv.style.width = "500px";
-  newDiv.style.height = "500px";
-  newDiv.style.background = "lime";
 };
 
 
