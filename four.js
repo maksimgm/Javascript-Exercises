b616bf63670de01c773a15258099bae8eb559937
maksimgm <maksimmamrikov@gmail.com>
2015-06-22 10:44:25 -0700

    add js and html content

---
+++ four.js
@@ -0,0 +1,9 @@
+var two = document.getElementById("two");
+
+document.onload = function myFunction() {
+  var p = document.createElement("p");
+  var t = document.createTextNode("I am the superior text");
+  p.appendChild(t);
+  document.getElementById("p");
+  document.two= p;
+};
