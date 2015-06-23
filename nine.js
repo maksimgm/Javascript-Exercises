490f52c0905c5309840cf10c86e3918659bd762a
maksimgm <maksimmamrikov@gmail.com>
2015-06-23 11:47:17 -0700

    add js and html content

---
+++ nine.js
@@ -0,0 +1,18 @@
+var StrValues = []
+StrValues [0] = "cricket";
+StrValues [1] = "basketball";
+StrValues [2] = "hockey";
+StrValues [3] = "swimming";
+StrValues [4] = "soccer";
+StrValues [5] = "tennis";
+
+
+
+
+/*var longestWord = "";
+
+for (var i = 0; i < StrValues.length; i++) {
+  if(StrValues[i].length > longestWord.length) {
+  }
+}
+*/
