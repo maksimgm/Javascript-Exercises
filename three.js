d8e7836960db5359a1c9d44cbe6330675a47b23d
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-06-23 15:10:04 -0700

    fixed js

--- three.js
+++ three.js
@@ -1,8 +1,8 @@
 var wrapper = document.getElementById("wrapper");
 
 wrapper.onclick = function MyFunc () {
-  document.getElementById("candy");
-  candy.innerHTML = "<h3> Because candy tastes amazing! </h3>";
+  // candy.innerHTML = "<h3> Because candy tastes amazing! </h3>";
+  var newClassName = wrapper.className + " thing";
+  wrapper.className = newClassName;
 };
-
 
