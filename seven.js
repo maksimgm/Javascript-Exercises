39167df1679256737c200aebb64a099609315c75
maksimgm <maksimmamrikov@gmail.com>
2015-06-22 19:09:06 -0700

    complete js document.

--- seven.js
+++ seven.js
@@ -1,8 +1,10 @@
-for ( var i = 1 ; i < 101 ; i+ ){
+for ( var i = 1 ; i < 100 ; i++ ){
+  if(i % 3 === 0)
   document.write(i + "<br>");
-  if ( i == 101) {
-    break;
+//  if ( i == 100) {
+  //  break;
   }
-}
+//}
 
-//page will not load.
+
+//for (var i = 3; i < 100; i += 3);
