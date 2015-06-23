dbeacb9126a24d1bf2a1ab141caa80714a13dc4b
maksimgm <maksimmamrikov@gmail.com>
2015-06-22 22:53:36 -0700

    complete task in js

--- four.js
+++ four.js
@@ -1,9 +1,12 @@
 var two = document.getElementById("two");
 
-document.onload = function myFunction() {
-  var p = document.createElement("p");
-  var t = document.createTextNode("I am the superior text");
-  p.appendChild(t);
-  document.getElementById("p");
-  document.two= p;
+window.onload = function() {
+  document.getElementById("two").innerHTML = "I am the superior text"
+
+
+ // var p = document.createElement("p");
+//  var t = document.createTextNode("I am the superior text");
+ // p.appendChild(t);
+ // document.getElementById("p");
+//  document.two= p;
 };
