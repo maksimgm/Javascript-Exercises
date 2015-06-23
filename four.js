73db26cb47cf56c335b31fb8d4c634c920604edf
maksimgm <maksimmamrikov@gmail.com>
2015-06-23 15:42:59 -0700

    add semicolon to statment

--- four.js
+++ four.js
@@ -1,9 +1,7 @@
 var two = document.getElementById("two");
 
 window.onload = function() {
-  document.getElementById("two").innerHTML = "I am the superior text"
-
-
+  document.getElementById("two").innerHTML = "I am the superior text";
  // var p = document.createElement("p");
 //  var t = document.createTextNode("I am the superior text");
  // p.appendChild(t);
