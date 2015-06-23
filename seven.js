ded87b927a80be9a2937f11da74e116f55704293
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-06-23 16:12:10 -0700

    update js

--- seven.js
+++ seven.js
@@ -1,10 +1,11 @@
 // METHOD #1
 
-for ( var i = 1 ; i < 100 ; i++ ){
-  if(i % 3 === 0)
-  document.write(i + "<br>");
-  if ( i == 100) {
-  //  break;
+for (var i = 1; i < 100; i++){
+  if (i % 3 === 0){
+    document.write(i + "<br>");
+  }
+  if (i % 5 === 0) {
+   document.write(i + "<br>"); 
   }
 }
 
