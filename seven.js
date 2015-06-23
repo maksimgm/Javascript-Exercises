8b0fee9733ee13ad93238df8edd360fb7d77a67e
maksimgm <maksimmamrikov@gmail.com>
2015-06-22 19:27:26 -0700

    add another method to solve example

--- seven.js
+++ seven.js
@@ -1,10 +1,15 @@
+// METHOD #1
+
 for ( var i = 1 ; i < 100 ; i++ ){
   if(i % 3 === 0)
   document.write(i + "<br>");
-//  if ( i == 100) {
+  if ( i == 100) {
   //  break;
   }
+}
+
+// METHOD #2
+
+//for( var i = 3; i < 100; i = i + 3){
+  //document.write(i + "<br>")
 //}
-
-
-//for (var i = 3; i < 100; i += 3);
