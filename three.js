19af17969e79695074cec699ff080460d2b484cf
maksimgm <maksimmamrikov@gmail.com>
2015-06-23 17:47:13 -0700

    add js content

--- three.js
+++ three.js
@@ -1,8 +1,6 @@
 var wrapper = document.getElementById("wrapper");
 
-wrapper.onclick = function MyFunc () {
-  // candy.innerHTML = "<h3> Because candy tastes amazing! </h3>";
+wrapper.onclick = function() {
   var newClassName = wrapper.className + " thing";
   wrapper.className = newClassName;
 };
-
