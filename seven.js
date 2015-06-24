54891486414cb9be0747b5cb72b963ddbc23968a
maksimgm <maksimmamrikov@gmail.com>
2015-06-23 19:08:27 -0700

    remove extra code

--- seven.js
+++ seven.js
@@ -1,16 +1,5 @@
-// METHOD #1
-
 for (var i = 1; i < 100; i++){
   if (i % 3 === 0){
     document.write(i + "<br>");
   }
-  if (i % 5 === 0) {
-   document.write(i + "<br>"); 
-  }
 }
-
-// METHOD #2
-
-//for( var i = 3; i < 100; i = i + 3){
-  //document.write(i + "<br>")
-//}
