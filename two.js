717e5eb9bb5b6b3c9304d11d0104941866c8f2a0
maksimgm <maksimmamrikov@gmail.com>
2015-07-01 16:45:23 -0700

    update readme.md

---
+++ two.js
@@ -0,0 +1,7 @@
+var pox = document.getElementById("pox");
+
+pox.onclick = function myFunction() {
+  document.getElementById("juice");
+  juice.innerHTML = ("This is the internet talking to you. ")
+};
+
