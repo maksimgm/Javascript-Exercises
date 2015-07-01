2015c37382c201629111cd1531ef236a453acd59
maksimgm <maksimmamrikov@gmail.com>
2015-07-01 16:31:40 -0700

    add to js doc

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,17 @@
-for( var i = 0; i < 101; i++ ) {
-  if( i % 3 === 0){
-    document.write( i + "<br>");
-    i.toString(2);
+for( var i = 1; i <= 100; i++ ) {
+  var divisibleByThree = i % 3 === 0;
+  var divisibleByFive = i % 5 === 0;
+
+  if( divisibleByThree) {
+    document.write("fizz" + "<br>");
+  }
+  if(divisibleByFive) {
+   document.write("buzz" + "<br>"); 
+  }
+  if(divisibleByThree && divisibleByFive) {
+    document.write("fizzbuzz" + "<br>");
+  }
+  else{
+    document.write(i + "<br>");
   }
 }
