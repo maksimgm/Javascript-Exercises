25940ecc088353c01178c5bcbcc6c080c97298f9
maksimgm <maksimmamrikov@gmail.com>
2015-07-02 15:15:05 -0700

    change if statement and var

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,14 @@
 for( var i = 1; i <= 100; i++ ) {
-  var divisibleByThree = i % 3 === 0;
-  var divisibleByFive = i % 5 === 0;
+  var divisibleByThree = i % 3;
+  var divisibleByFive = i % 5;
 
-  if( divisibleByThree) {
+  if( divisibleByThree === 0) {
     console.log("fizz");
   }
-  if(divisibleByFive) {
+  if(divisibleByFive === 0) {
    console.log("buzz"); 
   }
-  if(divisibleByThree && divisibleByFive) {
+  if(divisibleByThree && divisibleByFive === 0) {
     console.log("fizzbuzz");
   }
   else{
